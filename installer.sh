bc3c0c89fd9787a67a27fc98770a3bf95103d8e4
Belfagor2005 <72314956+Belfagor2005@users.noreply.github.com>
2025-03-06 16:06:18 +0100

    v.1.8

--- installer.sh
+++ installer.sh
@@ -2,8 +2,8 @@
 ## setup command=wget -q --no-check-certificate https://raw.githubusercontent.com/Belfagor2005/revolutionxxx/main/installer.sh -O - | /bin/sh
 
 ## Only This 2 lines to edit with new version ######
-version='1.6'
-changelog='\nAdd Locale Language\nFix conponent stream'
+version='1.8'
+changelog='\nFix conponent AV Swith'
 ##############################################################
 
 TMPPATH=/tmp/revolutionxxx-main
